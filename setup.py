16fd77ec46c5d0c7aac842ef3c0f78bc5174f3ab
Masoud Rafiei-Ravandi <mrafiei.ravandi@gmail.com>
2022-06-22 15:45:18 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+# Usage: python3 setup.py install --prefix=$HOME
+
+from setuptools import setup
+
+setup(name='frbx',
+      packages=['frbx'],
+      install_requires=['numpy',
+                        'scipy',
+                        'astropy',
+                        'matplotlib',
+                        'camb',
+                        'pyfftw',
+                        'healpy',
+                        'h5py',
+                        'multiprocess',
+                        'pathos',
+                        'handout',
+                        'corner',
+                        'requests',
+                        'chime_frb_api',
+                        'pymangle',
+                        'pygedm',
+                        'pytz'])
