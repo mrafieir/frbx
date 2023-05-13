7d0d03615acc22654a83c087f8d51d6d3bcc6491
Masoud Rafiei-Ravandi <mrafieir@users.noreply.github.com>
2023-05-12 19:27:13 -0700

    gal_dm test 1/2

--- tests/test-model.py
+++ tests/test-model.py
@@ -36,5 +36,35 @@ for i in range(n_iter):
 print('cl_models -> passed.')
 
 
+#########################################################################################
+#
+# gal_dm: (class) computes the galactic DMs for arrays of equatorial coordinates.
+
+pygedm = False
+nsides = [4, 8, 16]
+n_ra = 10
+n_dec = 10
+
+ret = np.zeros(2, 3, (len(nsides), n_ra, n_dec))
+
+for i, nside in enumerate(nsides):
+    gal_dm = fx.model_gal_dm(nside=nside)
+
+    for j, ra in enumerate(range(0.0, 360.0, n_ra)):
+        for k, dec in enumerate(range(-90.0, 90.0, n_dec)):
+            ret[0,0,i,j,k] = gal_dm(ra, dec, 'ymw16')
+            ret[0,1,i,j,k] = gal_dm(ra, dec, 'ymw16_hp')
+
+            ret[1,0,i,j,k] = gal_dm(ra, dec, 'ne01')
+            ret[1,1,i,j,k] = gal_dm(ra, dec, 'ne01_hp')
+
+            if pygedm:
+                ret[0,2,i,j,k] = gal_dm(ra, dec, 'pygedm_ymw16')
+                ret[1,2,i,j,k] = gal_dm(ra, dec, 'pygedm_ne01')
+
+#FIXME
+print('gal_dm -> passed.')
+
+
 print('test-model done!')
 print(f'Elapsed time: {(time.time()-start)/60.:.2f} min.')
